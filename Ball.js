20aa7d9091ba0f356233a260a057932806032672
procodingclass <88050205+procodingclass@users.noreply.github.com>
2021-12-05 11:19:28 +0530

    Add files via upload

--- Ball.js
+++ Ball.js
@@ -1,14 +1,10 @@
-
 class Ball {
   constructor(x, y, r) {
     var options = { 
-      density: 1, 
-      frictionAir: 0.005, 
-      restitution: 1.0
+      frictionAir: 0.005,
+      restitution : 1.0
     };
 
-    this.x = x;
-    this.y = y;
     this.body = Bodies.circle(x, y, r, options);
     this.radius = r;
     World.add(world, this.body);
@@ -16,10 +12,10 @@ class Ball {
 
 
   display() {
-    ellipse(this.body.position.x,this.body.position.y, this.radius, this.radius);
-  }
-
-  
+    
+    ellipse(this.body.position.x, this.body.position.y, this.radius, this.radius);
+    
+  } 
 }
 
 
