5b69bcc27c7356bcaaf795e8f05e69993929fc98
procodingclass <88050205+procodingclass@users.noreply.github.com>
2021-12-09 17:14:05 +0530

    Add files via upload

--- Box.js
+++ Box.js
@@ -2,22 +2,21 @@ class Box {
 
   constructor(x, y, w, h) {
     var options = {
+        frictionAir: 0.005,
         restitution:1.2,
-        density:0.04
     }
     this.body = Bodies.rectangle(x, y, w, h, options);
 
     this.width = w;
     this.height = h;
-    
+
     World.add(world, this.body);
   }
 
 
   display(){
-       
+
     rect(this.body.position.x, this.body.position.y, this.width, this.height);
-    
+
   }
 }
-
--- sketch.js
+++ sketch.js
@@ -9,24 +9,20 @@ var box1, box2, box3,ground;
 var ball;
 
 function setup() {
-  createCanvas(600, 400);
+  createCanvas(850, 400);
   engine = Engine.create();
   world = engine.world;
 
 
-ball = new Ball(50, 100, 40, 40);
-  
-console.log(ball);
+  ball = new Ball(50, 100, 30);
 
-  ground = new Box(0, 380, 600, 20);
-  
+  ground = new Box(0, 380,850, 20);
+
   Body.setStatic(ground.body, true);
-  
+
   box1 = new Box(100, 100, 70, 70);
   box2 = new Box(200, 100, 70, 70);
   box3 = new Box(300, 100, 70, 70);
-  
- 
 
 }
 
@@ -37,10 +33,6 @@ function draw() {
   box1.display();
   box2.display();
   box3.display();
-  
+
   ball.display();
 }
-
-
-
-
